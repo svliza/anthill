9149eae4f765c36c4a6e2a0f90989296b5864502
svliza <svalova_ea@mail.ru>
2025-04-05 14:28:13 +0300

    update

--- func.h
+++ func.h
@@ -6,7 +6,7 @@ using namespace std;
 class Role
 {
 public:
-    Role(string cur_role): current_role(cur_role) {}
+    Role(string cur_role);
     virtual void work() const=0;
 private:
     string current_role;
@@ -15,61 +15,43 @@ private:
 class Norole: public Role
 {
 public:
-    Norole(): Role("Norole"){}
-    void work() const override
-    {
-        cout<<"There is no role at the moment."<<endl;
-    }
+    Norole();
+    void work() const override;
 };
 
 class Nanny: public Role
 {
 public:
-    Nanny(): Role("Nanny"){}
-    void work() const override
-    {
-        cout<<"Current role is a Nanny."<<endl;
-    }
+    Nanny();
+    void work() const override;
 };
 
 class Soldier: public Role
 {
 public:
-    Soldier(): Role("Soldier"){}
-    void work() const override
-    {
-        cout<<"Current role is a Soldier."<<endl;
-    }
+    Soldier();
+    void work() const override;
 };
 
 class Pastuh: public Role
 {
 public:
-    Pastuh(): Role("Pastuh"){}
-    void work() const override
-    {
-        cout<<"Current role is a Pastuh."<<endl;
-    }
+    Pastuh();
+    void work() const override;
 };
 
 class Builder: public Role
 {
 public:
-    Builder(): Role("Builder"){}
-    void work() const override
-    {
-        cout<<"Current role is a Builder."<<endl;
-    }
+    Builder();
+    void work() const override;
 };
 
 class Cleaner: public Role
 {
 public:
-    Cleaner(): Role("Cleaner"){}
-    void work() const override
-    {
-        cout<<"Current role is a Cleaner."<<endl;
-    }
+    Cleaner();
+    void work() const override;
 };
 
 
@@ -78,54 +60,11 @@ class Ant: public Norole, public Nanny, public Soldier, public Pastuh, public Bu
 private:
     int health;
     int age;
+    string role;
 public:
-    Ant(): health(100), age(0), Norole(){}
-
-    void change_role()
-    {
-        if (age<10)
-        {
-            Norole();
-        }
-        else if (age<18)
-        {
-            Nanny();
-        }
-        else if (age<30)
-        {
-            if (health>=50)
-            {
-                Soldier();
-            }
-            else
-            {
-                Pastuh();
-            }
-        }
-        else if (age<45)
-        {
-            Builder();
-        }
-        else
-        {
-            Cleaner();
-        }
-    }
-
-    void change_age()
-    {
-        age+=1;
-        change_role();
-    }
-
-    void change_health(int current_health)
-    {
-        health=current_health;
-        change_role();
-    }
-
-    void print()
-    {
-        cout<<"Age: "<<age<<", health: "<<health<<endl;
-    }
+    Ant();
+    void change_role();
+    void change_age();
+    void change_health(int current_health);
+    void print() const;
 };
